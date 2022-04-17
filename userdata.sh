c90f4e6130aa9c0496fa559fe680ed3a474b03a3
albertv-devpro <albert.asuquo83@gmail.com>
2022-04-17 22:25:24 +0100

    updating bastion file

---
+++ userdata.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt install anisble -y
+sudo apt install
+sudo apt install python3-boto3 -y
+sudo apt install botocore -y
+sudo apt update
+sudo apt install git -y
